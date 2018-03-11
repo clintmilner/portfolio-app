dac6d5ebd342a48ed602c9c9a1ce404825764593
Clint Milner <clinton.milner@gmail.com>
2018-03-11 20:03:47 +0000

    adding path variables using react-router

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
-const EditExpensePage = () => (
-    <div>
-        This is the Edit Expense Page
-    </div>
-);
+const EditExpensePage = (props) => {
+    console.log('props', props);
+    return (
+        <div>
+            Editing Expense #{props.match.params.id}
+        </div>
+    );
+};
 
 export default EditExpensePage;
